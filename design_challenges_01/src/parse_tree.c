7a1155663033d7e0f51c9d7df8144f04e1c000c6
Jongkeun Choi <ynifamily3@gmail.com>
2019-03-29 15:54:17 +0900

    토큰분리 해결완료

--- design_challenges_01/src/parse_tree.c
+++ design_challenges_01/src/parse_tree.c
@@ -15,8 +15,9 @@ char operator_onebyte[NUMBER_OF_OPERATORS] = {(char)1, (char)2, (char)3, (char)4
 typedef struct {
 	char *stream;
 	int number_of_tokens; // 토큰 갯수
-	char *tokens[100]; //각 토큰들의 시작 위치 포인터
-	int *tokens_length[100]; //각 토큰들의 길이 (연산자는 무조건 1일 것이다..)
+	char *tokens[100]; // 각 토큰들의 시작 위치 포인터
+	int tokens_length[100]; // 각 토큰들의 길이 (연산자는 무조건 1일 것이다..)
+	bool is_operator[100]; // 토큰이 연산자인 경우에는 true, 아니면 false
 } _container;
 
 void normalize2(char *text)
@@ -39,7 +40,9 @@ void normalize2(char *text)
 
 void norm_onebyte_op(_container *container)
 {
+	// 에일리어싱
 	char *text = container->stream;
+
 	// 두 글자 이상의 연산자를 한 글자로 정규화한다. 입력 텍스트는 공백이 없다고 가정한다.
 	for (int i = 0; i < 16; i++) {
 		// 이 연산자 : operator[i] 를 찾아서 없을때까지 반복
@@ -50,11 +53,65 @@ void norm_onebyte_op(_container *container)
 			for (int j = 1; j < op_len; j++) pat_start[j] = ' ';
 		}
 	}
-	for (int i = 16; i < NUMBER_OF_OPERATORS; i++) {
-		// 원래 한 글자짜리 연산자이다.
-		// 토큰을 증가시키고 포인터를 증가시킨다.
-	}
+	// 연산자를 정규화하느라 생긴 공백을 다시 정규화한다.
 	normalize2(text);
+	
+	// 토큰을 분리한다. 초기 토큰 갯수는 0이라고 가정한다.
+	container->number_of_tokens = 0;
+	int len = strlen(text);
+	int pat_start_offset = 0;
+	int length_stack = 0;
+	for (int i = 0; i < len; i++) {
+		if (text[i] >= (char)1 && text[i] <= (char)16) {
+			// 정규화된 연산자.
+			if (length_stack > 0) {
+				// 그 전의 비-연산자 집합을 처리한다.
+				container->is_operator[container->number_of_tokens] = false;
+				container->tokens[container->number_of_tokens] = text + pat_start_offset;
+				container->tokens_length[container->number_of_tokens] = length_stack;
+				pat_start_offset += length_stack;
+				container->number_of_tokens++;
+				length_stack = 0;
+			}
+			// 해당 연산자를 처리
+			container->is_operator[container->number_of_tokens] = true;
+			container->tokens[container->number_of_tokens] = text + i; // 해당 연산자의 위치
+			container->tokens_length[container->number_of_tokens] = 1; // 연산자 길이는 1
+			container->number_of_tokens++; // 토큰 증가
+			pat_start_offset++; // 해당 연산자를 건너뛴 다음
+			continue;
+		}
+		switch (text[i])
+		{
+			// 원래 1-byte 연산자.
+			case '*':case '/':case '%':case '+':case '-':case '<':case '>':case '&':case '^':case '|':case '=': case '(': case ')':
+			if (length_stack > 0) {
+				container->is_operator[container->number_of_tokens] = false;
+				container->tokens[container->number_of_tokens] = text + pat_start_offset;
+				printf("오프셋 이동 : %d\n",  pat_start_offset); // 예상 6
+				container->tokens_length[container->number_of_tokens] = length_stack;
+				pat_start_offset += length_stack;
+				container->number_of_tokens++;
+				length_stack = 0;
+			}
+			// 해당 연산자처리
+			container->is_operator[container->number_of_tokens] = true;
+			container->tokens[container->number_of_tokens] = text + i; // 해당 연산자의 위치
+			container->tokens_length[container->number_of_tokens] = 1; // 연산자 길이는 1
+			container->number_of_tokens++; // 토큰 증가
+			pat_start_offset++; // 해당 연산자를 건너뛴 다음
+			continue;
+			break;
+		}
+		// 일반 문자열
+		++length_stack; //랭스택을 열심히 쌓는다.
+	}
+	// 남아있는 랭스택 처리
+	if (length_stack > 0) {
+		container->tokens[container->number_of_tokens] = text + pat_start_offset;
+		container->tokens_length[container->number_of_tokens] = length_stack;
+		container->number_of_tokens++;
+	}
 }
 
 void print_opop(const char *text)
@@ -130,7 +187,7 @@ int main(void)
 	_container container;
 	stack stk;
 	stk.top = -1;
-	char buf[] = "(a<b)&&(c>d)"; // raw Input
+	char buf[] = "(stop>signal1)&&(stop<signal2)"; // raw Input
 	char result[500]; // 후위표현식으로 전환된 문자열
 	container.stream = buf;
 	printf("%s\n", container.stream);
@@ -145,5 +202,12 @@ int main(void)
 
 	// 테스트용 출력
 	print_opop(buf);
+
+	// 테스트용 출력 2
+	printf("컨테이너에 분리된 토큰 개수 : %d\n", container.number_of_tokens);
+	for (int i = 0; i < container.number_of_tokens; i++) {
+		// printf("%d: [%ld, %d] ", container.is_operator[i], container.tokens[i] - container.stream, container.tokens_length[i]);
+		
+	}
 	return 0;
 }
